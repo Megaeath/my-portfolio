37cbaaa630353be61ff2876034928a107c460168
nirutk <nirutk@vgi.co.th>
2024-04-18 18:35:55 +0700

    Updates

---
+++ precache-manifest.f81c09c9f07ad1dd025b4cd7f7642191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24b07d19501229bc3b54d7c5ecb89a6",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d0ba6e9ed7487464565d",
+    "url": "/my-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "145f3f35d648e6579a0f",
+    "url": "/my-portfolio/static/css/main.6e3005a5.chunk.css"
+  },
+  {
+    "revision": "d0ba6e9ed7487464565d",
+    "url": "/my-portfolio/static/js/2.bc4ee523.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my-portfolio/static/js/2.bc4ee523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "145f3f35d648e6579a0f",
+    "url": "/my-portfolio/static/js/main.0110f81c.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
