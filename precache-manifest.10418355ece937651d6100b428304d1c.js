bcb7890a7cce43cf8260c6b0637619922e282afd
Auii <auii@Auiis-MacBook-Air.local>
2026-05-06 07:20:16 +0700

    Updates

---
+++ precache-manifest.10418355ece937651d6100b428304d1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef3b4c9b77890af78eaec389a43dcc6",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "2ca8fbd29cee6ec170ac",
+    "url": "/my-portfolio/static/css/2.896c4c41.chunk.css"
+  },
+  {
+    "revision": "6e0397f89d56e2aa19ca",
+    "url": "/my-portfolio/static/css/main.3905040c.chunk.css"
+  },
+  {
+    "revision": "2ca8fbd29cee6ec170ac",
+    "url": "/my-portfolio/static/js/2.bd7e7f4b.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/my-portfolio/static/js/2.bd7e7f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0397f89d56e2aa19ca",
+    "url": "/my-portfolio/static/js/main.d7bdeb48.chunk.js"
+  },
+  {
+    "revision": "f8bc21605bac90413ed8",
+    "url": "/my-portfolio/static/js/runtime-main.cc7e5888.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.2440c245d59ad42c6d24427baa9341a5.js"
+  "/my-portfolio/precache-manifest.10418355ece937651d6100b428304d1c.js"
 );
 
 self.addEventListener('message', (event) => {
